1f16a21beddc364f445947cbd2657aa86c0c60b7
Hector Franco <hector.franc0@hotmail.com>
2021-08-17 14:42:52 -0600

    Add project files.

---
+++ ClaseTeorica1208/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ClaseTeorica1208
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
